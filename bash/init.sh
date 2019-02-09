cd997c9d8be835988214a96177f52e4f092a93c8
Meck <>
2019-02-10 00:57:17 +0200

    1

--- bash/init.sh
+++ bash/init.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
+apt update
+apt install curl -y
 mkdir /home/init/
 cd /home/init
-wget -O deploy.sh https://github.com/flooriin-cloud/gcloud/blob/master/bash/deploy.sh
+curl -O -L https://raw.githubusercontent.com/flooriin-cloud/gcloud/master/bash/deploy.sh
 chmod u+x deploy.sh
-./deploy.sh
+./deploy.sh &>> output.txt
