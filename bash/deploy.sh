cf16a287118e2bc1167b4d55b86e5f44628fe730
Meck <>
2019-02-10 00:01:37 +0200

    1

--- bash/deploy.sh
+++ bash/deploy.sh
@@ -1,7 +1,43 @@
 #! /bin/bash
-apt update && apt upgrade -y
-apt install openjdk-8-jdk -y
-apt install python -y
+function package_exists() {
+    return dpkg -l "$1" &> /dev/null
+}
+
+apt update
+#apt upgrade -y
+if ! package_exists openjdk-8-jdk ; then
+    apt install openjdk-8-jdk -y
+fi
+apt install python less nano -y
+
+#Download Cassandra
+mkdir /home/init/
+cd /home/init
 wget http://apache.claz.org/cassandra/3.11.3/apache-cassandra-3.11.3-bin.tar.gz
-tar -xzvf apache-cassandra-3.11.2-bin.tar.gz
-mv apache-cassandra-3.11.2 /usr/local/cassandra
+tar -xzf apache-cassandra-3.11.3-bin.tar.gz
+mv -v apache-cassandra-3.11.3 cassandra
+mv -vf cassandra /usr/local/cassandra
+#Creating a Cassandra User
+useradd cassandra
+groupadd cassandra
+usermod -aG cassandra cassandra
+chown root:cassandra -R /usr/local/cassandra/
+chmod g+w -R /usr/local/cassandra/
+#Testing Permissions
+#su - cassandra
+#/usr/local/cassandra/bin/cassandra -f
+
+cat <<EOF > /etc/systemd/system/cassandra.service
+[Unit]
+Description=Cassandra Database Service
+After=network-online.target
+Requires=network-online.target
+[Service]
+User=cassandra
+Group=cassandra
+ExecStart=/usr/local/cassandra/bin/cassandra -f
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload && systemctl start cassandra.service && systemctl enable cassandra.service
--- init.sh
+++ init.sh
@@ -1,2 +1,7 @@
 #! /bin/bash
+less /var/log/apt/history.log
+
 gcloud compute --project=magnetic-math-210915 instance-templates create cass01 --machine-type=f1-micro --network=projects/magnetic-math-210915/global/networks/default --network-tier=PREMIUM --metadata=startup-script=\#\!\ /bin/bash$'\n'apt\ update\ \&\&\ apt\ upgrade\ -y$'\n'apt\ install\ openjdk-8-jdk\ -y$'\n'apt\ install\ python\ -y$'\n'wget\ http://apache.claz.org/cassandra/3.11.3/apache-cassandra-3.11.3-bin.tar.gz$'\n'tar\ -xzvf\ apache-cassandra-3.11.2-bin.tar.gz$'\n'mv\ apache-cassandra-3.11.2\ /usr/local/cassandra --maintenance-policy=MIGRATE --no-service-account --no-scopes --tags=https-server --image=ubuntu-minimal-1804-bionic-v20190204b --image-project=ubuntu-os-cloud --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=cass01
+
+gcloud compute --project=magnetic-math-210915
+      instances create cass01-1-1 --zone=us-east1-b --machine-type=f1-micro --subnet=default --network-tier=PREMIUM --metadata=startup-script=\#\!\ /bin/bash$'\n'apt\ update\ \&\&\ apt\ upgrade\ -y$'\n'apt\ install\ openjdk-8-jdk\ -y$'\n'apt\ install\ python\ -y$'\n'wget\ http://apache.claz.org/cassandra/3.11.3/apache-cassandra-3.11.3-bin.tar.gz$'\n'tar\ -xzvf\ apache-cassandra-3.11.2-bin.tar.gz$'\n'mv\ apache-cassandra-3.11.2\ /usr/local/cassandra --maintenance-policy=MIGRATE --no-service-account --no-scopes --tags=https-server --image=ubuntu-minimal-1804-bionic-v20190204b --image-project=ubuntu-os-cloud --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=cass01-1
